97ecb16dc77643b106e727806007eecaddeaf1e9
thvgger <thvgger005@gmail.com>
2025-10-16 11:09:39 +0100

    chore: update CORS configuration for frontend access

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import { cors } from "hono/cors";
 import { env } from "./env.js";
 
-const DEFAULT_ALLOWED_ORIGINS = ["http://localhost:4000", "*"];
+const DEFAULT_ALLOWED_ORIGINS = ["http://localhost:4000", "http://localhost:3000"];
 
 const allowedOrigins = env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
     ? env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
